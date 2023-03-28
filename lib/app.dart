63dc7f0c790de8b7a136cfb363f5b035da559ad2
Your Name <you@example.com>
2023-03-28 13:29:41 +0000

    AppBloc

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:states_bloc/blocs/navigate/navigate_bloc.dart';
-import 'package:states_bloc/blocs/request/request_bloc.dart';
-import 'package:states_bloc/blocs/slide/slide_bloc.dart';
-import 'package:states_bloc/blocs/text/text_bloc.dart';
+import 'package:states_bloc/blocs/app_bloc.dart';
 import 'package:states_bloc/core/app_router.dart';
 import 'package:states_bloc/core/keys.dart';
 import 'package:states_bloc/pages/home_page.dart';
@@ -14,23 +11,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider<NavigateBloc>(
-          create: (BuildContext context) =>
-              NavigateBloc(navigatorKey: Keys.navKey),
-        ),
-        BlocProvider<RequestBloc>(
-          create: (BuildContext context) => RequestBloc(),
-        ),
-        BlocProvider<SlideBloc>(
-          create: (BuildContext context) => SlideBloc(),
-        ),
-        BlocProvider<TextBloc>(
-          create: (BuildContext context) => TextBloc(
-            slideBloc: context.read<SlideBloc>(),
-          ),
-        ),
-      ],
+      providers: AppBloc.getInstance().getProviders(context), //[
+      //   appBloc.getProviders(context)
+      // BlocProvider<NavigateBloc>(
+      //   create: (BuildContext context) =>
+      //       NavigateBloc(navigatorKey: Keys.navKey),
+      // ),
+      // BlocProvider<RequestBloc>(
+      //   create: (BuildContext context) => RequestBloc(),
+      // ),
+      // BlocProvider<SlideBloc>(
+      //   create: (BuildContext context) => SlideBloc(),
+      // ),
+      // BlocProvider<TextBloc>(
+      //   create: (BuildContext context) => TextBloc(
+      //     slideBloc: context.read<SlideBloc>(),
+      //   ),
+      // ),
+      //],
       child: MaterialApp(
         navigatorKey: Keys.navKey,
         initialRoute: HomePage.routeName,
